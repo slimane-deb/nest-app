0b94319cdf81abebad3e57bab0f34ece8f292d91
slimane <cs_deb@esi.dz>
2020-06-24 14:35:03 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+      { cors: true }
+  );
+  app.setGlobalPrefix('/api');
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'client/dist/client'),
+    prefix: '/',
+  });
   await app.listen(3000);
 }
 bootstrap();
